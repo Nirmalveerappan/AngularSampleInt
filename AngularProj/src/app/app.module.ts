53fde8ec5bb9f2103a009a018601631f8eec53f2
Nirmalveerappan <33276100+Nirmalveerappan@users.noreply.github.com>
2017-12-17 17:51:43 +0530

    Updated Appmodule

--- AngularProj/src/app/app.module.ts
+++ AngularProj/src/app/app.module.ts
@@ -1,20 +1,29 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from './../environments/firebase.config';
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { routeValues } from "./app.routes";
 
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, 
     LoginComponent,
     DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    HttpModule,
+    RouterModule.forRoot(routeValues),
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
