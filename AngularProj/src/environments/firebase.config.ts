2a1fe15609c9d9a633447c7881bc9d4adf009ee1
Nirmalveerappan <33276100+Nirmalveerappan@users.noreply.github.com>
2017-12-17 17:36:18 +0530

    Create firebase config

---
+++ AngularProj/src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAPG8DaHT5moxdVwreh1nSHfZITjAOQxGs',
+    authDomain: 'sample-a1767.firebaseapp.com',
+    databaseURL: 'https://sample-a1767.firebaseio.com',
+    storageBucket: 'sample-a1767.appspot.com'
+  };
